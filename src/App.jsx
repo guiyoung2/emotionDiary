47cdbefd96b0453f50b9446cea518276024750f0
guiyoung2 <guiyoung22@naver.com>
2025-11-20 04:25:25 +0900

    버튼 생성 및 레이아웃까지

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
+import Button from "./components/Button";
+import Header from "./components/Header";
+
+import { getEmotionImage } from "./util/get-emotion-image";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -17,12 +21,34 @@ function App() {
 
   return (
     <>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+
+      <Button
+        text={"버튼"}
+        type={"default"}
+        onClick={() => {
+          console.log("버튼이 클릭되었습니다.");
+        }}
+      />
+      <Button
+        text={"버튼"}
+        type={"positive"}
+        onClick={() => {
+          console.log("버튼이 클릭되었습니다.");
+        }}
+      />
+      <Button
+        text={"버튼"}
+        type={"negative"}
+        onClick={() => {
+          console.log("버튼이 클릭되었습니다.");
+        }}
+      />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
