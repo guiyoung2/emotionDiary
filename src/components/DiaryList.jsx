51ec50c16f5d89d375c26391991e0500240dc92e
guiyoung2 <guiyoung22@naver.com>
2025-11-22 01:32:47 +0900

    Home 페이지 기능 추가(context, 날짜 정리, 데이터 수정

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -1,19 +1,47 @@
 import Button from "./Button";
 import DiaryItem from "./DiaryItem";
 import "./DiaryList.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-const DiaryList = () => {
+const DiaryList = ({ data }) => {
+  const nav = useNavigate();
+
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedData = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === "latest") {
+        return Number(b.createdDate) - Number(a.createdDate);
+      } else {
+        return Number(a.createdDate) - Number(b.createdDate);
+      }
+    });
+  };
+
+  const sortedData = getSortedData();
+
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
-          <option value={"lastest"}>최신순</option>
+        <select onChange={onChangeSortType} value={sortType}>
+          <option value={"latest"}>최신순</option>
           <option value={"oldest"}>오래된순</option>
         </select>
-        <Button text={"새 일기 작성"} type={"positive"} />
+        <Button
+          onClick={() => nav("/new")}
+          text={"새 일기 작성"}
+          type={"positive"}
+        />
       </div>
       <div className="list_wrapper">
-        <DiaryItem />
+        {sortedData.map((item) => (
+          <DiaryItem key={item.id} {...item} />
+        ))}
       </div>
     </div>
   );
